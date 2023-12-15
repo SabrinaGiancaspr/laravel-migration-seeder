528647c73aef8af60006b1f9d7916128d7642104
sabri <sabrinatwitter6@gmail.com>
2023-12-15 10:19:41 +0100

    edit

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -17,13 +17,13 @@ class TrainSeeder extends Seeder
         for ($i = 0; $i < 20; $i++) {
             $new_train = new Train();
 
-            $new_train->azienda = $faker->company;
-            $new_train->stazione_di_partenza = $faker->city;
-            $new_train->stazione_di_arrivo = $faker->city;
+            $new_train->azienda = $faker->randomElement(['Italo', 'Trenitalia', 'DeutscheBahn']);
+            $new_train->stazione_di_partenza = $faker->city();
+            $new_train->stazione_di_arrivo = $faker->city();
             $new_train->orario_partenza = $faker->dateTimeThisMonth('+12 days')->format('H:i:s');
             $new_train->orario_arrivo = $faker->dateTimeThisMonth('+14 days')->format('H:i:s');
             $new_train->data_partenza = $faker->dateTimeThisMonth('+12 days')->format('Y-m-d');
-            $new_train->codice_treno = $faker->numberBetween(0, 100);
+            $new_train->codice_treno = $faker->regexify('[A-Z]{5}[0-4]{3}');
             $new_train->numero_carrozze = $faker->numberBetween(7, 15);
             $new_train->in_orario = $faker->boolean;
             $new_train->cancellato = $faker->boolean;
